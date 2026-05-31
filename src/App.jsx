f7cc394d05fd078ca95e50e1be6d72925655b57b
Esraa Jebara <52330587@students.liu.edu.lb>
2026-05-31 23:14:59 +0300

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/home';
-import Menu from './pages/Menu';
+import Menu from './pages/menu';
 import About from './pages/About';
 import Reservation from './pages/Reservation';
 import './index.css';
